6863bb45b8550cde2b9c832eea99bddc0fe22486
Christian Stocker <me@chregu.tv>
2010-12-16 10:17:47 +0100

    A simple versioning example

---
+++ examples/Versioning-Client.java
@@ -0,0 +1,53 @@
+
+import javax.jcr.Credentials;
+import javax.jcr.Node;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.version.Version;
+
+import javax.jcr.version.VersionHistory;
+import javax.jcr.version.*;
+import javax.jcr.version.VersionManager;
+import ch.liip.jcr.davex.DavexClient;
+
+public class Client {
+	
+	
+	public static void main(String[] args) {
+		try {
+			
+			String url = "http://localhost:8081/server/";
+			String workspace = "default";
+			
+			DavexClient Client = new DavexClient(url);
+			Repository repo = Client.getRepository();
+			Credentials sc = new SimpleCredentials("admin","admin".toCharArray());
+			Session s = repo.login(sc,workspace);
+			Node t;
+			Node root = s.getRootNode();
+			if (s.nodeExists("/foo")) {
+				t = root.getNode("foo");
+			} else {
+				t = root.addNode("foo");
+			}
+			t.addMixin("mix:versionable");
+			VersionManager vm = s.getWorkspace().getVersionManager();
+			vm.checkout("/foo");
+			
+			t.setProperty("bar","foo");
+			s.save();
+			Version v = vm.checkin("/foo");
+			
+			VersionHistory history = vm.getVersionHistory("/foo");
+			for (VersionIterator it = history.getAllVersions(); it.hasNext();) {
+				Version version = (Version) it.next();
+				System.out.println(version.getCreated().getTime());
+			}
+			
+		} catch (RepositoryException e) {
+			e.printStackTrace();
+		}
+	}
+}
