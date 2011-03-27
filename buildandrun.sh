840d99272fabb7f1c72f6c29c84659720481cb6c
Christian Stocker <me@chregu.tv>
2011-03-27 21:07:10 +0200

    Use 2.2.5

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JACKRABBIT_JAR=/opt/git/jackrabbit/jackrabbit-standalone/target/jackrabbit-standalone-2.2.1-SNAPSHOT.jar
+JACKRABBIT_JAR=/opt/git/jackrabbit/jackrabbit-standalone/target/jackrabbit-standalone-2.2.5-SNAPSHOT.jar
 
 javac  -Xlint:deprecation  -cp  $JACKRABBIT_JAR src/Client.java src/ch/liip/jcr/davex/DavexClient.java  src/ch/liip/jcr/davex/RepositoryConfigImplTest.java src/ch/liip/jcr/davex/AbstractRepositoryConfig.java   
 cd src 
